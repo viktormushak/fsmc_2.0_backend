eb07ed50168d29a48f898d4fd656a638f2a31981
Viktor_M <viktor.mushak@gmail.com>
2019-09-07 23:16:06 +0300

    Add user

--- src/main/java/com/fsmc/backend/configurations/AppSecurityConfiguration.java
+++ src/main/java/com/fsmc/backend/configurations/AppSecurityConfiguration.java
@@ -14,8 +14,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
-
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {

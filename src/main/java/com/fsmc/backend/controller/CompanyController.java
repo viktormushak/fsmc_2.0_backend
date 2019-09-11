c2956c070a9dfdefa8750837830d9d3d9c4be975
Viktor_M <viktor.mushak@gmail.com>
2019-09-11 13:34:45 +0300

    some message

--- src/main/java/com/fsmc/backend/controller/CompanyController.java
+++ src/main/java/com/fsmc/backend/controller/CompanyController.java
@@ -1,5 +1,6 @@
 package com.fsmc.backend.controller;
 
+import com.fsmc.backend.data.model.Company;
 import com.fsmc.backend.data.network.Result;
 import com.fsmc.backend.data.repo.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
@@ -20,7 +23,7 @@ public class CompanyController {
     }
 
     @GetMapping
-    public Result getCompanies(OAuth2Authentication authentication){
-        return new Result.Success<>(companyRepository.getCompaniesByResponsibleUsername((String) authentication.getPrincipal()));
+    public List<Company> getCompanies(OAuth2Authentication authentication){
+        return companyRepository.getCompaniesByResponsibleUsername((String) authentication.getPrincipal());
     }
 }
